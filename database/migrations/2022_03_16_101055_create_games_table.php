5f313f68cedac89c11087146f5f4f5797e068bb4
Nail-catcher <nikita-mokin@mail.ru>
2022-03-18 10:49:48 +0300

    frstcmm

---
+++ database/migrations/2022_03_16_101055_create_games_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->date('date')->nullable();
+            $table->string('lol_id')->nullable();
+            $table->text('statistic')->nullable();
+            $table->foreignId('user_id')->nullable()->constrained('users')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
---
+++ database/migrations/2022_03_16_104124_create_highlights_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHighlightsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('highlights', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('game_id')->nullable()->constrained('games')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->text('link');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('highlights');
+    }
+}
